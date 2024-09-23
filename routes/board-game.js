34a2f6cef3945a5bcb342a4c7880ac64d0d84835
Po Cheng Lo <zxc45544@gmail.com>
2024-09-23 10:03:13 +0800

    新增分頁

--- routes/board-game.js
+++ routes/board-game.js
@@ -5,10 +5,34 @@ const router = express.Router();
 
 // 路由處理器
 router.get("/", async (req, res) => {
-  const sql = "SELECT * FROM prod_list ";
+  // 分頁 (查詢字串qs: page=2&perpage=5) (目前第page頁，每頁perpage筆資料)
+  // 公式: limit = perpage
+  //      offset = (page-1) * perpage
+  const page = Number(req.query.page) || 1; // page 預設為 1
+  const perpage = Number(req.query.perpage) || 12; // perpage 預設為 10
+  const limit = perpage;
+  const offset = (page - 1) * perpage;
+
+  const sql = `SELECT * FROM prod_list LIMIT ${limit} OFFSET ${offset}`;
   const [rows] = await db.query(sql);
+
+  // 進行分頁時，額外執行sql在此條件下總共多少筆資料
+  const [rows2] = await db.query(`SELECT COUNT(*) AS count FROM prod_list`);
+  const { count } = rows2[0];
+  // 計算總頁數
+  const pageCount = Math.ceil(count / perpage);
+
   // rows 讀取的資料
-  res.json(rows);
+  res.json({
+    status: "success",
+    data: {
+      total: count, //總筆數
+      pageCount, // 總頁數
+      page, // 目前頁
+      perpage, // 每筆頁數
+      rows,
+    },
+  });
 });
 
 router.get("/:prod_id", async (req, res) => {
