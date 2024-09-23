9610e710d3b8d25dc1994c7cef25a71b5b731f26
Zizi <rongnong2324@gmail.com>
2024-09-23 16:52:13 +0800

    調整id欄位名稱

--- routes/larp.js
+++ routes/larp.js
@@ -7,19 +7,18 @@ const router = express.Router();
 router.get("/:larpid", async (req, res) => {
   const { larpid } = req.params;
 
-  const sql = "SELECT * FROM larp_list WHERE larp_type=1 AND larp_id=?";
+  const sql = "SELECT * FROM larp_list WHERE larp_type=1 AND id=?";
   const [rows] = await db.query(sql, [larpid]);
   // rows 讀取的資料
   res.json(rows);
 });
 
 router.get("/", async (req, res) => {
-  
   const sql = `SELECT *
   FROM larp_list ll
-  JOIN larp_loc lc ON ll.larp_id = lc.larp_id
-  WHERE ll.larp_type = 1`;
-  
+  JOIN larp_loc lc ON ll.id = lc.id
+  WHERE ll.larp_type = 1
+  `;
   // const sql = `SELECT ll.larp_id, ll.larp_img, ll.larp_name, ll.larp_price
   // FROM larp_list ll
   // JOIN larp_loc lc ON ll.larp_id = lc.larp_id
