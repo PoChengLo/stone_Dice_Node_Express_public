8de7958b881a191690d6189660df70796186d5e2
Po Cheng Lo <zxc45544@gmail.com>
2024-09-27 13:45:35 +0800

    新增價格排序，關鍵字查詢

--- routes/board-game.js
+++ routes/board-game.js
@@ -5,6 +5,65 @@ const router = express.Router();
 
 // 路由處理器
 router.get("/", async (req, res) => {
+  // where條件 ---- START
+  const conditions = [];
+  const search = [];
+
+  // 關鍵字 (查詢字串qs: prod_name_like='%${prod_name_like}%')
+  const prod_name_like = req.query.prod_name_like || "";
+  if (prod_name_like)
+    search.push(`prod_name LIKE ${db.escape("%" + prod_name_like + "%")} `);
+  const prod_desc_like = req.query.prod_desc_like || "";
+  if (prod_desc_like)
+    search.push(`prod_desc LIKE ${db.escape("%" + prod_desc_like + "%")} `);
+  const prod_intro_like = req.query.prod_intro_like || "";
+  if (prod_intro_like)
+    search.push(`prod_intro LIKE ${db.escape("%" + prod_intro_like + "%")} `);
+  const prod_rules_like = req.query.prod_rules_like || "";
+  if (prod_rules_like)
+    search.push(`prod_rules LIKE ${db.escape("%" + prod_rules_like + "%")} `);
+
+  const search_key =
+    search.length > 0 ? search.map((v) => ` ${v} `).join(` OR `) : "";
+
+  // 如果 search_keys 存在，將其加入條件中
+  if (search_key) {
+    conditions[0] = `${search_key}`;
+  }
+
+  // 價格, 1100~2800 (查詢字串QS: price_min=1100&price_max=2800)
+  const price_min = Number(req.query.price_min) || 1100; // 最小價格
+  const price_max = Number(req.query.price_max) || 2800; // 最大價格
+  conditions[1] = `price BETWEEN ${price_min} AND ${price_max}`;
+
+  // 以下開始組合where從句
+  // 1. 過濾有空白條件情況
+  const search_condition = conditions.filter((v) => v);
+  // 2. 用AND來串接所有條件(注意csv中如果是空陣列時不要套用where)
+  const where =
+    search_condition.length > 0
+      ? "WHERE " + search_condition.map((v) => `( ${v} )`).join(` AND `)
+      : "";
+
+  // where條件 ---- END
+
+  let orderBy2 = "";
+  const orderby = req.query.orderby;
+  switch (orderby) {
+    case "id_asc":
+      orderBy2 = " ORDER BY id ASC";
+      break;
+    case "id_desc":
+      orderBy2 = " ORDER BY id DESC";
+      break;
+  }
+  // 排序，例如價格由低到高 (查詢字串qs: sort=price&order=asc) (順向asc，逆向desc)
+  const sort = req.query.sort || "id"; // 預設的排序資料表欄位
+  const order = req.query.order || "ASC"; //預設使用順向 asc (1234..)
+
+  // 建立sql從句字串
+  const orderBy = `ORDER BY ${sort} ${order}`;
+
   // 分頁 (查詢字串qs: page=2&perpage=5) (目前第page頁，每頁perpage筆資料)
   // 公式: limit = perpage
   //      offset = (page-1) * perpage
@@ -13,7 +72,9 @@ router.get("/", async (req, res) => {
   const limit = perpage;
   const offset = (page - 1) * perpage;
 
-  const sql = `SELECT * FROM prod_list LIMIT ${limit} OFFSET ${offset}`;
+  const sql = `SELECT * FROM prod_list ${where} ${orderBy} LIMIT ${limit} OFFSET ${offset}`;
+  console.log({ sql });
+
   const [rows] = await db.query(sql);
 
   // 進行分頁時，額外執行sql在此條件下總共多少筆資料
@@ -22,7 +83,7 @@ router.get("/", async (req, res) => {
   // 計算總頁數
   const pageCount = Math.ceil(count / perpage);
 
-  // rows 讀取的資料
+  // 回傳的的資料，使用json格式
   res.json({
     status: "success",
     data: {
@@ -31,6 +92,7 @@ router.get("/", async (req, res) => {
       page, // 目前頁
       perpage, // 每筆頁數
       rows,
+      where,
     },
   });
 });
@@ -43,7 +105,7 @@ router.get("/:id", async (req, res) => {
   try {
     // 使用動態路由
     const [row] = await db.query(sql, [id]);
-    return res.json({ status: "success", data: { row } });
+    res.json({ status: "success", data: { row } });
   } catch (error) {
     res.status(500).json({ error: "Database query error" });
   }
---
+++ sql/board-game.sql
@@ -0,0 +1,19 @@
+-- 預設排序 id
+SELECT * FROM prod_list LIMIT 8 OFFSET 0;
+
+-- 關鍵字搜尋 (查詢字串qs: prod_name_like=台灣)
+SELECT * FROM prod_list WHERE  prod_name LIKE '%台灣%' OR prod_desc  LIKE '%開心%' OR prod_intro  LIKE '%有趣%' OR prod_rules  LIKE '%角色%';
+
+-- 價格區間 例如5000~15000  (查詢字串qs: price_gte=5000&price_lte=15000)
+-- 使用 AND 與 大於小於符號
+SELECT * FROM prod_list WHERE price >= 1600 AND price <= 1800 ORDER BY price DESC;
+
+-- 整合測試(每個條件使用AND相連，會讓資料集合愈來愈少)
+SELECT * FROM prod_list WHERE prod_name LIKE '%魔法%' AND price >= 2000 AND price <= 2500;
+
+
+-- 最低1100 最高2800
+SELECT * FROM prod_list ORDER BY price ASC;
+
+-- 測試
+SELECT pl.*,tl.tag_name FROM prod_list pl  JOIN prod_tag pt ON pl.id = pt.id JOIN tag_list tl ON pt.tag_id = tl.tag_id;
